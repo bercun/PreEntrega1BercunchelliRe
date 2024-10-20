85c12cc1909acb5d8133d1301f376ff6352a355f
bercun <bercun@gmail.com>
2024-10-20 02:34:59 -0300

    dando estilos a NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@
 
 function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3 ">
             <a className="navbar-brand" href="/">El Obrador</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -21,6 +21,21 @@ function NavBar() {
                     <li className="nav-item">
                         <a className="nav-link" href="/category/3">Un poco de nuestra historia</a>
                     </li>
+                    <li className="nav-item style = display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #F2F2F2;
+  width: 90vw;
+  height: 80vh;
+  max-width: 600px;
+  max-height: 600px;
+  border: 1px solid black;
+  border-radius: 25px;
+  margin: auto;
+  padding: 15px;
+  overflow: auto;">
+                            <a className="nav-link" href="#" ><i className="fa-solid fa-cart-shopping"></i> <span className="badge bg-danger rounded-pill " id="carrito-box">0</span></a>
+                        </li>
                 </ul>
             </div>
         </nav>
