f1d09030e87e6d3cd7665cc6fc7caa205ca075ff
bercun <bercun@gmail.com>
2024-10-20 17:18:04 -0300

    add img en app.css a productos

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,29 +2,36 @@
 
 function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-light p-3 ">
-            <a className="navbar-brand" href="/">El Obrador</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">inicio</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/category/1">Recetas 1</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/category/2">Login</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/category/3">Un poco de nuestra historia</a>
-                    </li>
-                    <li className="nav-item">
-                            <a className="nav-link  " href="#" ><i className="fa-solid fa-cart-shopping"></i> <span className="badge bg-danger rounded-pill " id="carrito-box">0</span></a>
+        <nav className="navbar navbar-expand-sm  navbar-dark">
+            
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#"><img src="./public/img/icono_obrador.png" alt="icono sin fondo"/>El Obrador</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavbar">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse " id="collapsibleNavbar">
+                    <ul className="navbar-nav ">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Home</a>
                         </li>
-                </ul>
+                        <li className="nav-item">
+                            <a className="nav-link" href="./pages/productos.html">Productos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="./pages/recetas.html">Recetas</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="./pages/login.html">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="./pages/about.html">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="./pages/carrito.html"><i className="fa-solid fa-cart-shopping"></i> <span className="badge bg-danger rounded-pill " id="carrito-box">1</span></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
